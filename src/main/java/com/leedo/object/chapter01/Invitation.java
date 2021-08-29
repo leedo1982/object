6787a46bd15e92e419005140166dc4c34ae9d4eb
Leedo <leedo@deali.net>
2021-08-29 15:21:52 +0900

    step01

---
+++ src/main/java/com/leedo/object/chapter01/Invitation.java
@@ -0,0 +1,12 @@
+package com.leedo.object.chapter01;
+
+import java.time.LocalDateTime;
+
+/**
+ * 초대장
+ */
+public class Invitation {
+
+    // 초대일자
+    private LocalDateTime when;
+}
---
+++ src/main/java/com/leedo/object/chapter01/Ticket.java
@@ -0,0 +1,10 @@
+package com.leedo.object.chapter01;
+
+public class Ticket {
+
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
